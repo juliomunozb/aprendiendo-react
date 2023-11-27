19b2589c05e3b13dbe620de2f1a2e097aa45665c
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-27 16:36:09 -0500

    02-project: Encontrando el ganador del juego y bloquear el tablero para no permitir seguir jugando

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -17,23 +17,57 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 function App () {
   // Crea un array y signa el estado inicial con todos los valores a null
   const [board, setBoard] = useState(Array(9).fill(null))
   // Creando estado para saber a quien le toca el turno
   const [turn, setTurn] = useState(TURNS.X)
 
+  // null no hay ganador, false hay un empate
+  const [winner, setWinner] = useState(null)
+
   const updateBoard = (index) => {
     // No actualizar posicion si ya se tiene algo
-    if (board[index]) return
+    if (board[index] || winner) return
 
-    // Copiando el table para mostrar visualmente la jugada
+    // Actualizar tableto para mostrar visualmente la jugada
     const newBoard = [...board] // Se hace copia del board por que no se deberia mutar el estado original.
     newBoard[index] = turn // Asignando valor X u O
     setBoard(newBoard)
 
+    const newWinner = checkWinter(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+    }
+
+    // Cambiar el turno
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+  }
+
+  const checkWinter = (boardToCheck) => {
+    // Revisamos todas las combinaciones ganadores
+    // para ver si X u O ganó
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (boardToCheck[a] && // X u O
+          boardToCheck[a] === boardToCheck[b] &&
+          boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a] // Retorna X u O
+      }
+    }
+    return null
   }
   return (
     <main className='board'>
