65512e8b20652647292adcfffe5284994d784eea
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-27 15:10:55 -0500

    02-project: Creando tablero de juego

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,6 +1,40 @@
-function App () {
+import { useState } from 'react'
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+// Cuadrado de cada posicion del tablero
+const Square = ({ children, isSelected, updateBoard, index }) => {
   return (
-    <h1>Tic Tact Toe</h1>
+    <div className='square'>
+      {children}
+    </div>
+  )
+}
+
+function App () {
+  // Crea un array y signa el estado inicial con todos los valores a null
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  return (
+    <main className='board'>
+      <h1>Tic Tact Toe</h1>
+      <section className='game'>
+        {
+          board.map((cell, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
