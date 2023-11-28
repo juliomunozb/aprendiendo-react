2f5be462ce2f564f82b54d8b953e67aa3b0bdaa8
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-28 16:54:07 -0500

    03-project: Agrengado useState para activar o desactivar circulo del mouse

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [enable, setEnable] = useState(false)
+
+  useEffect(() => {
+    console.log('Effect', { enable });
+  }, [enable])
 
   return (
-    <h1>Proyecto 3</h1>
+    <main>
+      <h1>Proyecto 3</h1>
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? 'Desactivar' : 'Activar'} Seguir puntero
+      </button>
+    </main>
+
   )
 }
 
