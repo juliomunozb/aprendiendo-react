4581d5c9057fab33f77ffe9970cd3288a9f06531
Julio Muñoz <ing.juliomb@gmail.com>
2023-11-30 14:05:52 -0500

    04-project: Separando el llamada a las API en diferentes useEffect

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -7,29 +7,36 @@ export function App () {
   const [fact, setFact] = useState()
   const [image, setImage] = useState()
 
+  // para recuperar la cita al cargar la página
   useEffect(() => {
+    console.log('useEffect Cita', { fact })
     const getRandomFact = async () => {
       try {
         const response = await fetch(CAT_ENDPOINT_RANDOM_CAT)
         const data = await response.json()
         const { fact } = data
         setFact(fact)
-        const threeFirstWords = fact.split(' ', 3).join(' ')
-        console.log(threeFirstWords)
-        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-          .then(res => res.json())
-          .then(response => {
-            const { _id } = response
-            const url = `cat/${_id}/says/${threeFirstWords}`
-            setImage(url)
-            console.log(response)
-          })
       } catch (error) {
         console.log('Error Invocando API', error.message)
       }
     }
     getRandomFact()
   }, [])
+
+  // para recuperar la imagen cada vez que se tiene una cita nueva
+  useEffect(() => {
+    console.log('useEffect Imagen', { fact })
+    if (!fact) return
+    const threeFirstWords = fact.split(' ', 3).join(' ')
+    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
+      .then(res => res.json())
+      .then(response => {
+        const { _id } = response
+        const url = `cat/${_id}/says/${threeFirstWords}`
+        setImage(url)
+      })
+  }, [fact])
+
   return (
     <main>
       <h1>App Gatos</h1>
