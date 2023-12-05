c30ea166c3cb14927f16db8d59f192e9496e6378
Julio Muñoz <ing.juliomb@gmail.com>
2023-12-05 12:49:45 -0500

    05-project: Leyendo json que retorna las movies y renderizandolo en el componente

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,6 +1,10 @@
+import responseMovies from './mocks/with-result.json'
+// import withoutResult from './mocks/no-result.json'
 import './App.css'
 
 function App () {
+  const movies = responseMovies.Search
+  const hasmovies = movies?.length > 0
   return (
     <div className='page'>
       <header>
@@ -11,7 +15,21 @@ function App () {
         </form>
       </header>
       <main>
-        Render peliculas
+        {hasmovies
+          ? (
+            <ul>
+              {
+                movies.map(movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title} />
+                  </li>
+                ))
+              }
+            </ul>
+            )
+          : (<p>No se encontraron películas para esta busqueda</p>)}
       </main>
     </div>
   )
