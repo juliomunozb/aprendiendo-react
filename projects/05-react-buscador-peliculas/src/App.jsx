e99a63a8827c32b609f66dd13e89f1266003cd70
Julio Muñoz <ing.juliomb@gmail.com>
2023-12-06 16:09:01 -0500

    05-project: validando formulario de forma controlada usando useEffect

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,28 +1,51 @@
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App () {
   const { movies } = useMovies()
-
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
   const handleSubmit = (event) => {
     event.preventDefault()
-    // Captura la data para varios input
-    const fields = Object.fromEntries(new window.FormData(event.target))// target se refiere al evento, al propio formulario
-    console.log(fields)
+    console.log({ query })
   }
+
+  const handleOnchange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  useEffect(() => {
+    if (query === '') {
+      setError('No se puede buscar una película vacía')
+      return
+    }
+    if (query.length < 3) {
+      setError('La busqueda debe tener más de 3 caractéres')
+      return
+    }
+    // Expresion regular para validar solo numeros
+    // ^  : Coincide con el inicio de la cadena
+    // \d : Busca cualquier dígito (número arábigo). Equivalente a [0-9]
+    // +$ : Coincide con el inicio de la cadena
+    if (query.match(/^\d+$/)) {
+      setError('No se permiten busquedas de solo numeros')
+      return
+    }
+    setError(null)
+  }, [query])
 
   return (
     <div className='page'>
       <header>
         <h1>Buscador de películas</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input name='query' type='text' placeholder='Avenger, Star Wars, The Matrix,..' />
-          <input name='query2' type='text' placeholder='Avenger, Star Wars, The Matrix,..' />
-          <input name='query3' type='text' placeholder='Avenger, Star Wars, The Matrix,..' />
+          <input onChange={handleOnchange} type='text' placeholder='Avenger, Star Wars, The Matrix,..' />
           <button type='submit'> Buscar</button>
         </form>
       </header>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
       <main>
         <Movies movies={movies} />
       </main>
