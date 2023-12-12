5505b9746fc492576237a8956b29cfe0378b43ac
Julio Muñoz <ing.juliomb@gmail.com>
2023-12-12 16:29:23 -0500

    05-project: usando useRef para guardar la busqueda anterior y evitar que se invoque nuevamente el API si se hace la misma busqueda

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { searchMovies } from '../services/movies.js'
 
 export function useMovies ({ search }) {
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(false)
   const [errors, setErrors] = useState(null)
+  const previousSearch = useRef(search)
 
   const getMovies = async () => {
+    if (search === previousSearch.current) return
     try {
+      previousSearch.current = search
       setLoading(true)
       const newMovies = await searchMovies({ search })
       setMovies(newMovies)
