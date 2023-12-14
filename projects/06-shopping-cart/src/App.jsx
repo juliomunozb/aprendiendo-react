a774974c2d60a0659788e5167f843d33af8863ef
Julio Muñoz <ing.juliomb@gmail.com>
2023-12-14 17:07:12 -0500

    06-project: Mostrar listar de productos desde json

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,6 +1,11 @@
+import { Products } from './components/Products'
+import { products } from './mocks/products.json'
 function App () {
   return (
-    <h1>Shopping cart Shopping Cart 🛒 </h1>
+    <>
+      <h1>Shopping cart Shopping Cart 🛒</h1>
+      <Products products={products} />
+    </>
   )
 }
 
