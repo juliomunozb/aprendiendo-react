384fde197ba8333ef7898b822c30e8f3f547712b
Julio Muñoz <ing.juliomb@gmail.com>
2023-12-15 10:17:06 -0500

    06-project: incluyendo logica para friltros - sin componente grafico

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react'
 import { Products } from './components/Products'
-import { products } from './mocks/products.json'
+import { products as inititalProducts } from './mocks/products.json'
 function App () {
+  const [products] = useState(inititalProducts)
+  const [filter, setFilter] = useState({
+    category: 'smartphones',
+    price: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filter.price &&
+        (filter.category === 'all' ||
+        product.category === filter.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
   return (
     <>
       <h1>Shopping cart Shopping Cart 🛒</h1>
-      <Products products={products} />
+      <Products products={filteredProducts} />
     </>
   )
 }
