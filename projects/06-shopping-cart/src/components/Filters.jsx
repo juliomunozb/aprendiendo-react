45f98dbd08f45e1efafe1a578273da409d366669
Julio Muñoz <ing.juliomb@gmail.com>
2023-12-15 10:43:50 -0500

    06-project: mostrando valor del range en la interfaz

--- projects/06-shopping-cart/src/components/Filters.jsx
+++ projects/06-shopping-cart/src/components/Filters.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react'
 import './Filters.css'
 export function Filters () {
+  const [minPrice, setMinPrice] = useState(0)
+  const handleChangeMinPrice = (event) => {
+    setMinPrice(event.target.value)
+  }
   return (
     <section className='filters'>
       <div>
-        <label htmlFor='price'>Precio</label>
+        <label htmlFor='price'>Precio a partir de:</label>
         <input
           type='range'
           id='price'
           min='0'
           max='1000'
+          onChange={handleChangeMinPrice}
         />
+        <span>{minPrice}</span>
       </div>
       <div>
         <label htmlFor='category'> Categorías</label>
