90270865ab38ab99c3428855d34db719562d4bd9
Julio Muñoz <ing.juliomb@gmail.com>
2023-12-19 15:02:54 -0500

    06-project: cuando se agrega producto al carrito se cambia icono de add por remove

--- projects/06-shopping-cart/src/components/Products.jsx
+++ projects/06-shopping-cart/src/components/Products.jsx
@@ -1,23 +1,35 @@
 import '../components/Products.css'
 import { useCart } from '../hooks/useCart'
-import { AddToCartIcon } from './Icons'
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons.jsx'
 export function Products ({ products }) {
-  const { addToCart } = useCart()
+  const { addToCart, cart } = useCart()
+  const checkProductInCar = product => {
+    return cart.some(item => item.id === product.id)
+  }
   return (
     <main className='products'>
       <ul>
         {
-          products.map(product => (
-            <li key={product.id}>
-              <img src={product.thumbnail} alt={product.title} />
-              <div>
-                <strong>{product.title} - {product.price}</strong>
-              </div>
-              <div>
-                <button onClick={() => addToCart(product)}><AddToCartIcon /></button>
-              </div>
-            </li>
-          ))
+          products.map(product => {
+            const isProductInCart = checkProductInCar(product)
+            return (
+              <li key={product.id}>
+                <img src={product.thumbnail} alt={product.title} />
+                <div>
+                  <strong>{product.title} - {product.price}</strong>
+                </div>
+                <div>
+                  <button onClick={() => addToCart(product)}>
+                    {
+                      isProductInCart
+                        ? <RemoveFromCartIcon />
+                        : <AddToCartIcon />
+                    }
+                  </button>
+                </div>
+              </li>
+            )
+          })
         }
       </ul>
     </main>
