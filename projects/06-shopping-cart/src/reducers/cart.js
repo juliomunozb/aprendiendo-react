16f9fe84efd45f989568793f33babd123d534736
Julio Muñoz <ing.juliomb@gmail.com>
2023-12-23 14:32:03 -0500

    06-project: crean objeto de acciones en el cartReducer para hacerlo mas visible y escalable

--- projects/06-shopping-cart/src/reducers/cart.js
+++ projects/06-shopping-cart/src/reducers/cart.js
@@ -6,45 +6,43 @@ export const CART_ACTIONS_CART = {
   REMOVE_FROM_CART: 'REMOVE_FROM_CART',
   CLEAR_CART: 'CLEAR_CART'
 }
+const UPDATE_STATE_BY_ACTION = {
+  [CART_ACTIONS_CART.ADD_TO_CART]: (state, action) => {
+    const { payload: actionPayload } = action
+    const { id } = actionPayload
+    const productInCartIndex = state.findIndex(item => item.id === id)
+    if (productInCartIndex >= 0) {
+      // Copia profunda del objeto
+      const newState = structuredClone(state)
+      newState[productInCartIndex].quantity += 1
+      updateCartToStorage(newState)
+      return newState
+    }
+
+    const newState = [
+      ...state,
+      {
+        ...actionPayload,
+        quantity: 1
+      }
+    ]
+    updateCartToStorage(newState)
+    return newState
+  },
+  [CART_ACTIONS_CART.REMOVE_FROM_CART]: (state, action) => {
+    const { id } = action.payload
+    const newState = state.filter(item => item.id !== id)
+    updateCartToStorage(newState)
+    return newState
+  },
+  [CART_ACTIONS_CART.CLEAR_CART]: () => {
+    resetCartToStorage()
+    return []
+  }
+}
 
 export const cartReducer = (state, action) => {
-  console.log('entra cartReducer')
-  const { type: actionType, payload: actionPayload } = action
-  switch (actionType) {
-    case CART_ACTIONS_CART.ADD_TO_CART:{
-      const { id } = actionPayload
-      const productInCartIndex = state.findIndex(item => item.id === id)
-      if (productInCartIndex >= 0) {
-        // Copia profunda del objeto
-        const newState = structuredClone(state)
-        newState[productInCartIndex].quantity += 1
-        updateCartToStorage(newState)
-        return newState
-      }
-
-      const newState = [
-        ...state,
-        {
-          ...actionPayload,
-          quantity: 1
-        }
-      ]
-      updateCartToStorage(newState)
-      return newState
-    }
-
-    case CART_ACTIONS_CART.REMOVE_FROM_CART:{
-      const { id } = actionPayload
-      const newState = state.filter(item => item.id !== id)
-      updateCartToStorage(newState)
-      return newState
-    }
-
-    case CART_ACTIONS_CART.CLEAR_CART:{
-      resetCartToStorage()
-      return []
-    }
-  }
-
-  return state
+  const { type: actionType } = action
+  const updateState = UPDATE_STATE_BY_ACTION[actionType]
+  return updateState ? updateState(state, action) : state
 }
