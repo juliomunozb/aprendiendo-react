760844449840180fd92fe303d6c18c5cbf6b1d17
Julio Muñoz <ing.juliomb@gmail.com>
2024-01-16 17:53:58 -0500

    07-project: Agregando validación para abrir link con opciones de teclado

--- projects/07-midu-router/src/Link.jsx
+++ projects/07-midu-router/src/Link.jsx
@@ -12,10 +12,16 @@ export function navigate (href) {
 
 export function Link ({ target, to, ...props }) {
   const handleClick = (event) => {
-    // Omite el compatamiento del elemento a
-    // No se refresca toda la pagina
-    event.preventDefault()
-    navigate(to)
+    const isMainEvent = event.button === 0 // botón redecho del mouse
+    const isModifiedEvent = event.metaKey || event.altKey || event.ctrlKey || event.shiftKey // si se oprimen teclar Mayusculas, alt, ctrl, shift
+    const isManageableEvent = target === undefined || target === '_self' // si enlace se abre en la misma ventana
+
+    if (isMainEvent && isManageableEvent && !isModifiedEvent) {
+      // Omite el compatamiento del elemento a
+      // No se refresca toda la pagina
+      event.preventDefault()
+      navigate(to) // Navegación con SPA
+    }
   }
   return <a onClick={handleClick} href={to} target={target} {...props} />
 }
