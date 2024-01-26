08944946040c81dff89881286c84a3e05925bfa5
Julio Muñoz <ing.juliomb@gmail.com>
2024-01-26 16:11:03 -0500

    07-project: donfiguracion archivo package.json para publicar paquete en NPM

---
+++ projects/07-midu-router/lib/src/index.js
@@ -0,0 +1,1 @@
+export{Router}from"./components/Router.jsx";export{Route}from"./components/Route.jsx";export{Link}from"./components/Link.jsx";
---
+++ projects/07-midu-router/src/index.jsx
@@ -0,0 +1,3 @@
+export { Router } from './components/Router.jsx'
+export { Route } from './components/Route.jsx'
+export { Link } from './components/Link.jsx'
