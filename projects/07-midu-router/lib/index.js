d9e56ab4e27910c8e3f4322e603bdb8286c6ed53
Julio Muñoz <ing.juliomb@gmail.com>
2024-01-29 15:53:55 -0500

    07-project: Funcionalidad de obtener el queryString y publivcar nueva version en NPM

--- projects/07-midu-router/lib/index.js
+++ projects/07-midu-router/lib/index.js
@@ -1,1 +1,1 @@
-export{Router}from"./components/Router.jsx";export{Route}from"./components/Route.jsx";export{Link}from"./components/Link.jsx";
+export{Router}from"./components/Router.jsx";export{Route}from"./components/Route.jsx";export{Link}from"./components/Link.jsx";export function useQueryParams({req}={}){const isServer=typeof window==="undefined";if(isServer){const{query}=req;return query}const search=window.location.search;const params=new URLSearchParams(search);return Object.fromEntries(params.entries())}
--- projects/07-midu-router/src/pages/Search.jsx
+++ projects/07-midu-router/src/pages/Search.jsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react'
-
+import { useQueryParams } from '../index.jsx'
 export function Search ({ routeParams }) {
+  const { limit } = useQueryParams()
   useEffect(() => {
     document.title = `Has buscado ${routeParams.query}`
   }
   , [])
   return (
-    <h1>Has buscado {routeParams.query}</h1>
+    <h1>Has buscado {routeParams.query} - {limit}</h1>
   )
 }
