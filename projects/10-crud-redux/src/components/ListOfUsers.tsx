d2b1507185a7cfccdef73d362b6b35dc4b2a0964
Julio Muñoz <ing.juliomb@gmail.com>
2024-02-14 12:57:39 -0500

    10-project: Tipando correctamente el store

--- projects/10-crud-redux/src/components/ListOfUsers.tsx
+++ projects/10-crud-redux/src/components/ListOfUsers.tsx
@@ -11,10 +11,10 @@ import {
   Badge,
 } from '@tremor/react'
 
-import { useSelector } from 'react-redux'
+import { useAppSelector } from '../hooks/store'
 
 export function ListOfUsers() {
-  const users = useSelector(state => state.users)
+  const users = useAppSelector(state => state.users)
   return (
     <>
       <Card>
