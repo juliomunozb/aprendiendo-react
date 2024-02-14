4ba3b1cec1098e72de0a0cb07d9b9109e6922899
Julio Muñoz <ing.juliomb@gmail.com>
2024-02-14 11:07:36 -0500

    10-project: SLICE. Organizando el store, creando slice para users

---
+++ projects/10-crud-redux/src/store/users/slice.ts
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export interface users {
+  name: string
+  email: string
+  github: string
+}
+
+export interface UserWhithId extends users {
+  id: string
+}
+
+const initialState: UserWhithId[] = [
+  {
+    id: '1',
+    name: 'yazmanito',
+    email: 'yazmanito@gmail.com',
+    github: 'yamanito',
+  },
+  {
+    id: '2',
+    name: 'John Doe',
+    email: 'leo@gmail.com',
+    github: 'leo',
+  },
+  {
+    id: '3',
+    name: 'Julio Mendez',
+    email: 'Julio@gmail.com',
+    github: 'juliomunozb',
+  },
+]
+
+export const userSlice = createSlice({
+  name: 'users',
+  initialState,
+  reducers: {},
+})
+
+export default userSlice.reducer
