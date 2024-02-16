6a1584fc4668cb311ecc0beed126dc1c3df30622
Julio Muñoz <ing.juliomb@gmail.com>
2024-02-16 17:50:23 -0500

    10-project: validando formulario y mostrando errores

--- projects/10-crud-redux/src/components/CreateNewUser.tsx
+++ projects/10-crud-redux/src/components/CreateNewUser.tsx
@@ -1,17 +1,25 @@
-import { Card, Button, Title, TextInput } from '@tremor/react'
+import { Badge, Card, Button, Title, TextInput } from '@tremor/react'
 import { useUserActions } from '../hooks/useUserActions'
-import React from 'react'
+import React, { useState } from 'react'
 
 export function CreateNewUser() {
   const { addUser } = useUserActions()
+  const [result, setResult] = useState<'ok' | 'ko' | null>(null)
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
+    setResult(null)
     const form = event.target as HTMLFormElement
     const formData = new FormData(form)
     const name = formData.get('name') as string
     const email = formData.get('email') as string
     const github = formData.get('github') as string
+
+    if (!name || !email || !github) {
+      return setResult('ko')
+    }
     addUser({ name, email, github })
+    setResult('ok')
+    form.reset()
   }
   return (
     <Card style={{ marginTop: '16px' }}>
@@ -24,6 +32,12 @@ export function CreateNewUser() {
           <Button type='submit' style={{ marginTop: '16px' }}>
             Crear usuario
           </Button>
+          <span>
+            {result === 'ok' && (
+              <Badge color='green'>Guardado correctamente</Badge>
+            )}
+            {result === 'ko' && <Badge color='red'>Error en los campos</Badge>}
+          </span>
         </div>
       </form>
     </Card>
