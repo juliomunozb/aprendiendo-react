f27d28eec6a038212cf7cacef26fdbf23dad7c1d
Julio Muñoz <ing.juliomb@gmail.com>
2024-02-19 14:15:57 -0500

    10-project: agregando paquete para mostrar notificaciones

--- projects/10-crud-redux/src/App.tsx
+++ projects/10-crud-redux/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { CreateNewUser } from './components/CreateNewUser'
 import { ListOfUsers } from './components/ListOfUsers'
+import { Toaster } from 'sonner'
 function App() {
   return (
     <>
       <ListOfUsers />
       <CreateNewUser />
+      <Toaster richColors />
     </>
   )
 }
