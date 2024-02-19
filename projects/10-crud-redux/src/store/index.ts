e608f41dc79e9949d579543c9a5c02acce958bca
Julio Muñoz <ing.juliomb@gmail.com>
2024-02-19 15:43:31 -0500

    10-project: middleware para sincronizar borrado de usuario en la base de datos

--- projects/10-crud-redux/src/store/index.ts
+++ projects/10-crud-redux/src/store/index.ts
@@ -1,17 +1,43 @@
 import { configureStore, Tuple, type Middleware } from '@reduxjs/toolkit'
-import usersReducer from './users/slice'
+import usersReducer, { userID } from './users/slice'
+import { toast } from 'sonner'
+
+export type Action = { type: 'users/deleteUserById'; payload: userID }
 
 const persistanceLocalStorageMiddleware: Middleware =
   store => next => action => {
     next(action)
     localStorage.setItem('_redux_state_', JSON.stringify(store.getState()))
   }
+// Se refleja el borrado del usurio en la interfaz
+// Posteriormente se hace el sincronizado con base de datos
+const syncWithDatabaseMiddleware: Middleware = store => next => action => {
+  const { type, payload } = action as Action
+
+  //Fase 1
+  next(action)
+  if (type === 'users/deleteUserById') {
+    fetch(`https://jsonplaceholder.typicode.com/users/${payload}`, {
+      method: 'DELETE',
+    })
+      .then(res => {
+        if (res.ok) {
+          toast.success(`Usuario ${payload} borrado correctamente`)
+        }
+      })
+      .catch(err => {
+        console.log(err)
+        console.log('Error')
+      })
+  }
+}
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
   },
-  middleware: () => new Tuple(persistanceLocalStorageMiddleware),
+  middleware: () =>
+    new Tuple(persistanceLocalStorageMiddleware, syncWithDatabaseMiddleware),
 })
 
 export type RootState = ReturnType<typeof store.getState>
