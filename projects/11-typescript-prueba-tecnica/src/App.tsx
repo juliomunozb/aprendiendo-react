c5b93ed1954e895553b28876510cb65c068aee96
Julio Muñoz <ing.juliomb@gmail.com>
2024-02-23 15:39:33 -0500

    11-project: Fech para consumir API de usuarios

--- projects/11-typescript-prueba-tecnica/src/App.tsx
+++ projects/11-typescript-prueba-tecnica/src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { type User } from './types'
 
 function App() {
+  const [users, setUsers] = useState<User[]>([])
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUsers(res.results)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
   return (
     <>
       <h1>Prueba técnica</h1>
+      {JSON.stringify(users)}
     </>
   )
 }
