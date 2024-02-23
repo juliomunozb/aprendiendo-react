914f676fb1b14d2f72632d9a3e0bc3cd3858beff
Julio Muñoz <ing.juliomb@gmail.com>
2024-02-23 14:37:07 -0500

    11-project: configuración inicial

---
+++ projects/11-typescript-prueba-tecnica/.eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+  },
+}
---
+++ projects/11-typescript-prueba-tecnica/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Prueba técnica</h1>
+    </>
+  )
+}
+
+export default App
