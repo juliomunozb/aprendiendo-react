af7a5327cd11e485ed49d50eec9a617c788b825b
Julio Muñoz <ing.juliomb@gmail.com>
2024-03-05 15:38:09 -0500

    11b-project: refactorizando. Instalando tanstact/react-query y creaando el provider

--- projects/11b-typescript-prueba-tecnica/src/main.tsx
+++ projects/11b-typescript-prueba-tecnica/src/main.tsx
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
 )
