e2dd925e782c3c517ec6a01ff32dbf610d2354d5
Julio Muñoz <ing.juliomb@gmail.com>
2024-03-12 10:59:11 -0500

    11b-project: DevTools (Funcionalidad fetching). Agregando un timeout antes de hacer el fetch

--- projects/11b-typescript-prueba-tecnica/src/services/users.ts
+++ projects/11b-typescript-prueba-tecnica/src/services/users.ts
@@ -1,4 +1,7 @@
+const delay = async (ms: number) =>
+  await new Promise(resolve => setTimeout(resolve, ms))
 export const fetchUsers = async ({ pageParam = 1 }: { pageParam: number }) => {
+  await delay(1000)
   return await fetch(
     `https://randomuser.me/api/?results=10&seed=users&page=${pageParam}`
   )
