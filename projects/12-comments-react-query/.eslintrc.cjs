5c2a703d170b3f35872430c79443f566e7fb8f56
Julio Muñoz <ing.juliomb@gmail.com>
2024-03-14 17:51:13 -0500

    12-project: configuración inicial

---
+++ projects/12-comments-react-query/.eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react'],
+  rules: {
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'space-before-function-paren': 'off',
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/no-unsafe-argument': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/method-signature-style': 'off',
+    '@typescript-eslint/non-nullable-type-assertion-style': 'off',
+  },
+}
