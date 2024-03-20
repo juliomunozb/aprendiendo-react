9933535c64e4f295e448e42e2f277179a6f54ad5
Julio Muñoz <ing.juliomb@gmail.com>
2024-03-20 11:39:41 -0500

    12-project: maquetacion inicial

--- projects/12-comments-react-query/src/App.tsx
+++ projects/12-comments-react-query/src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
 function App() {
   return (
-    <>
-      <h1>Send comments</h1>
-    </>
+    <main>
+      <div className='comments'>
+        <ul className='list-comments'>
+          <li>coment1</li>
+          <li>coment2</li>
+        </ul>
+      </div>
+      <div className='create-comments'>
+        <form action=''>
+          <div>
+            <label htmlFor='title'></label>
+            <input type='text' id='title' placeholder='title' />
+          </div>
+          <div>
+            <textarea placeholder='comment'></textarea>
+          </div>
+          <button>Send comment</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
